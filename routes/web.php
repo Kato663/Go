0d992de98e164fad28db058a511a611f67cc1436
Kato Goro <m01411027@iot.ac.jp>
2021-03-12 07:10:34 +0000

    PHP/laravel12課題3

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::group(['prefix' => 'admin'], function() {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 Auth::routes();
 
