76f76c3aa95cccf628519f655855e3577b2c99c1
Kato Goro <m01411027@iot.ac.jp>
2021-03-16 17:27:34 +0000

    PHP/laravel14課題5

--- app/News.php
+++ app/News.php
@@ -13,3 +13,16 @@ class News extends Model
     	'body' => 'required',
     	);
 }
+
+class profiles extends Model
+{
+    protected $guarded = array('id');
+
+    // 以下を追記
+    public static $rules = array(
+        'name' => 'required',
+        'gender' => 'required',
+        'hobby' => 'required',
+        'introduction' => 'required',
+    );
+}
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -24,30 +24,29 @@
                     @endif
                     <!--ifからendifまで、まだ良く分からない-->
             <div class="form-group row">
-                <label class="col-md-2">氏名</label>
+                <label class="col-md-2" for="name">氏名</label>
                 <div class="col-md-10">
                     <input type="text" class="form-control" name="name" value="{{ old('name')}}">
                     <!--タイトルの横につくしろいところoldでエラーが出たときに前の文章が保存される？-->
                 </div>
             </div>
             
-            <div class="form-goup row">
-                <label class="col-md-2">性別</label>
+            <div class="form-group row">
+                <label class="col-md-2" for="gender">性別</label>
                 <div class="col-md-10">
                     <input type="text" class="form-control" name="gender" value="{{ old('name')}}">
                 </div>
             </div>
             
-            
             <div class="form-group row">
-                <label class="col-md-2">趣味</label>
+                <label class="col-md-2" for="hobby">趣味</label>
                 <div class="col-md-10">
                     <input type="text" class="form-control" name="hobby" value="{{old('hobby')}}">
                 </div>
             </div>
             
             <div class="form-group row">
-                <lavel class="col-md-2">自己紹介</lavel>
+                <lavel class="col-md-2" for="introduction">自己紹介</lavel>
                  <div class="col-md-10">
                             <textarea class="form-control" name="introduction" rows="15">{{ old('introduction') }}</textarea>
                         </div>
