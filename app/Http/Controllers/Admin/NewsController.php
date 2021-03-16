cc237899043afc8621370f1fd33f08a6716407b6
Kato Goro <m01411027@iot.ac.jp>
2021-03-16 12:27:17 +0000

    PHP/laravel14課題前修正１

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// 以下を追記することでNews Modelが扱えるようになる
+use App\News;
+
 class NewsController extends Controller
 {
   //
